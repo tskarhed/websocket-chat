1f897b6a93c1961c7efa8153b93da9b748e3c9fc
Tobias Skarhed <tobias.skarhed@gmail.com>
2020-05-29 11:19:22 +0200

    Custom header path

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Who am I?'));
+app.get('/test', (req, res) => {
+    res.set('X-Custom-header', "I'm a teapot damnit!");
+    res.status(418);
+    return res.send();
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
